2f101cd342f61bcf541f12abb3b35553ee77f759
back2nix <back2nix@list.ru>
2023-10-01 13:36:50 +0300

    make push

--- home.nix
+++ home.nix
@@ -276,6 +276,14 @@ in
       ch = "stat --format '%a'";
       cdspeak = "cd ~/Documents/code/github.com/back2nix/speaker";
       cdgo = "cd ~/Documents/code/github.com/back2nix";
+      # search Files and Edit
+      fe = "rg --files ''\${1:-.} | fzf --preview 'bat -f {}' | xargs $EDITOR";
+      # Search content and Edit
+      se = ''        fileline = $(rg - n ''${1:-.} | fzf | awk '{print $1}' | sed 's/.$//')
+                    $EDITOR ''${fileline%%:*} +''${fileline##*:}
+      '';
+      # fl = ''git log --oneline --color=always | fzf --ansi --preview=" echo { } | cut - d ' ' - f 1 | xargs - I @ sh -c 'git log --pretty=medium -n 1 @; git diff @^ @' | bat --color=always" | cut -d ' ' -f 1 | xargs git log --pretty=short -n 1'';
+      gd = "git diff --name-only --diff-filter=d $@ | xargs bat --diff";
       cdnix = "cd ~/Documents/code/github.com/back2nix/nix/my-nix-config-huawei";
       cdinfo = "cd ~/Documents/code/github.com/back2nix/info";
       clip = "head -c -1|xclip -i -selection clipboard";
@@ -352,7 +360,8 @@ in
     #
     #   difftool = {
     #     prompt = false;
-    #     "difftastic".cmd = ''difft "$LOCAL" "$REMOTE"'';
+    #     "difftastic".cmd = ''
+    # difft "$LOCAL" "$REMOTE" '';
     #   };
     # };
   };
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,13 +1,14 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  config,
-  pkgs,
-  ...
-}: let
+{ config
+, pkgs
+, ...
+}:
+let
   user = "bg";
-in {
+in
+{
   imports = [
     # Include the results of the hardware scan.
     #<home-manager/nixos>
@@ -18,9 +19,9 @@ in {
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
 
-  boot.supportedFilesystems = ["ntfs"];
+  boot.supportedFilesystems = [ "ntfs" ];
 
-  services.xserver.videoDrivers = ["modesetting"];
+  services.xserver.videoDrivers = [ "modesetting" ];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -104,6 +105,8 @@ in {
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  services.lorri.enable = true; # replace default nix-shell
+
   # Enable sound with pipewire.
   sound.enable = true;
   # pavucontol for settings loop back "Monitor of Alder Lake PCH-P High Definition Audio Controller HDMI / DisplayPort 3 Output"
@@ -133,7 +136,7 @@ in {
   users.users.${user} = {
     isNormalUser = true;
     description = "${user}";
-    extraGroups = ["networkmanager" "wheel" "docker" "podman" "input" "audio"]; #
+    extraGroups = [ "networkmanager" "wheel" "docker" "podman" "input" "audio" ]; #
     # openssh = {
     #   authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnLD+dQsKPhCV3eY0lMUP4fDrECI1Boe6PbnSHY+eqRpkA/Nd5okdyXvynWETivWsKdDRlT3gIVgEHqEv8s4lzxyZx9G2fAgQVVpBLk18G9wkH0ARJcJ0+RStXLy9mwYl8Bw8J6kl1+t0FE9Aa9RNtqKzpPCNJ1Uzg2VxeNIdUXawh77kIPk/6sKyT/QTNb5ruHBcd9WYyusUcOSavC9rZpfEIFF6ZhXv2FFklAwn4ggWzYzzSLJlMHzsCGmkKmTdwKijkGFR5JQ3UVY64r3SSYw09RY1TYN/vQFqTDw8RoGZVTeJ6Er/F/4xiVBlzMvxtBxkjJA9HLd8djzSKs8yf amnesia@amnesia"];
     # };
@@ -157,7 +160,7 @@ in {
     ];
   };
 
-  environment.shells = with pkgs; [zsh];
+  environment.shells = with pkgs; [ zsh ];
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   # environment.binsh = "${pkgs.dash}/bin/dash";
@@ -176,7 +179,7 @@ in {
   systemd.tmpfiles.rules = [
     "d /var/lib/bluetooth 700 root root - -"
   ];
-  systemd.targets."bluetooth".after = ["systemd-tmpfiles-setup.service"];
+  systemd.targets."bluetooth".after = [ "systemd-tmpfiles-setup.service" ];
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -215,7 +218,7 @@ in {
 
   networking.nat = {
     enable = true;
-    internalInterfaces = ["ve-+"];
+    internalInterfaces = [ "ve-+" ];
     externalInterface = "wlp0s20f3";
     # Lazy IPv6 connectivity for the container
     enableIPv6 = true;
@@ -238,67 +241,67 @@ in {
     localAddress = "192.168.100.3";
     hostAddress6 = "fc00::1";
     localAddress6 = "fc00::2";
-    config = {
-      config,
-      pkgs,
-      ...
-    }: {
-      # environment.systemPackages = with pkgs; [
-      #   dante
-      #  ];
-      services._3proxy = {
-        # https://nixos.wiki/wiki/3proxy
-        # https://github.com/3proxy/3proxy/wiki/How-To-(incomplete)#BIND
-        enable = true;
-        services = [
-          {
-            type = "socks";
-            auth = ["none"];
-            acl = [
-              {
-                rule = "allow";
-                users = ["test1"];
-              }
-            ];
-          }
-        ];
-        usersFile = "/etc/3proxy.passwd";
-      };
-
-      environment.etc = {
-        "3proxy.passwd".text = ''
-          test1:CL:password1
-          test2:CR:$1$rkpibm5J$Aq1.9VtYAn0JrqZ8M.1ME.
-        '';
-      };
-
-      networking.wg-quick.interfaces = {
-        wg0 = {
-          address = ["10.8.0.7/24"];
-          dns = ["1.1.1.1"];
-          privateKeyFile = "/home/${user}/.ssh/wireguard-keys/private";
-
-          peers = [
+    config =
+      { config
+      , pkgs
+      , ...
+      }: {
+        # environment.systemPackages = with pkgs; [
+        #   dante
+        #  ];
+        services._3proxy = {
+          # https://nixos.wiki/wiki/3proxy
+          # https://github.com/3proxy/3proxy/wiki/How-To-(incomplete)#BIND
+          enable = true;
+          services = [
             {
-              publicKey = "HiSr0nPcBXkTzYpySK7B0rJwGM0LLXbehwTncBCPYhI=";
-              presharedKeyFile = "/home/${user}/.ssh/wireguard-keys/presharedKeyFile";
-              allowedIPs = ["0.0.0.0/0"];
-              # endpoint = "194.28.224.146:51820";
-              endpoint = "166.1.160.225:51820";
-              persistentKeepalive = 0;
+              type = "socks";
+              auth = [ "none" ];
+              acl = [
+                {
+                  rule = "allow";
+                  users = [ "test1" ];
+                }
+              ];
             }
           ];
+          usersFile = "/etc/3proxy.passwd";
         };
-      };
 
-      system.stateVersion = "23.05";
+        environment.etc = {
+          "3proxy.passwd".text = ''
+            test1:CL:password1
+            test2:CR:$1$rkpibm5J$Aq1.9VtYAn0JrqZ8M.1ME.
+          '';
+        };
 
-      networking.firewall = {
-        # enable = true;
-        allowedTCPPorts = [53 80 433 1080 51820];
+        networking.wg-quick.interfaces = {
+          wg0 = {
+            address = [ "10.8.0.7/24" ];
+            dns = [ "1.1.1.1" ];
+            privateKeyFile = "/home/${user}/.ssh/wireguard-keys/private";
+
+            peers = [
+              {
+                publicKey = "HiSr0nPcBXkTzYpySK7B0rJwGM0LLXbehwTncBCPYhI=";
+                presharedKeyFile = "/home/${user}/.ssh/wireguard-keys/presharedKeyFile";
+                allowedIPs = [ "0.0.0.0/0" ];
+                # endpoint = "194.28.224.146:51820";
+                endpoint = "166.1.160.225:51820";
+                persistentKeepalive = 0;
+              }
+            ];
+          };
+        };
+
+        system.stateVersion = "23.05";
+
+        networking.firewall = {
+          # enable = true;
+          allowedTCPPorts = [ 53 80 433 1080 51820 ];
+        };
+        # environment.etc."resolv.conf".text = "nameserver 8.8.8.8";
       };
-      # environment.etc."resolv.conf".text = "nameserver 8.8.8.8";
-    };
   };
 
   # Open ports in the firewall.
