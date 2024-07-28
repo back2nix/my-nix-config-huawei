9f5a29a187f76e18524f4bb7dd6ed4aeeaefad5c
back2nix <back2nix@list.ru>
2024-07-29 00:24:35 +0300

    save tmux new config

--- module/users/bg/tmux/tmux.nix
+++ module/users/bg/tmux/tmux.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
   programs.tmux = {
     enable = true;
+    keyMode = "vi";
+    customPaneNavigationAndResize = true;
+    aggressiveResize = false;
+    historyLimit = 100000;
+    resizeAmount = 5;
+    escapeTime = 0;
+    secureSocket = false;
     clock24 = true;
     baseIndex = 1;
     prefix = "C-a";
@@ -8,195 +15,56 @@
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "screen-256color";
 
+    plugins = with pkgs; [
+      tmuxPlugins.copycat
+      tmuxPlugins.sensible
+      tmuxPlugins.yank
+      tmuxPlugins.vim-tmux-navigator
+      # tmuxPlugins.fingers
+      # tmuxPlugins.net-speed
+      # tmuxPlugins.cpu
+      # tmuxPlugins.battery
+      # {
+      #   plugin = tmuxPlugins.battery;
+      #   # extraConfig = ''
+      #   #   set-option -g status-right '#[fg=#5c6370, bg=#282c34, nobold, nounderscore, noitalics] batt: #{battery_percentage} '
+      #   # '';
+      # }
+      # tmuxPlugins.nord
+      tmuxPlugins.onedark-theme
+    ];
+
     extraConfig = ''
-      # Feel free to NOT use this variables at all (remove, rename)
-      # this are named colors, just for convenience
-      color_orange="colour166" # 208, 166
-      color_purple="colour134" # 135, 134
-      color_green="colour076" # 070
-      color_blue="colour39"
-      color_yellow="colour220"
-      color_red="colour160"
-      color_black="colour232"
-      color_white="white" # 015
+      set -g mouse on
+      unbind -n MouseDrag1Pane
+      bind -n MouseDrag1Pane if -F '#{mouse_any_flag}' 'if -F "#{pane_in_mode}" "copy-mode -M" "send-keys -M"' 'copy-mode -M'
 
-      # This is a theme CONTRACT, you are required to define variables below
-      # Change values, but not remove/rename variables itself
-      color_dark="$color_black"
-      color_light="$color_white"
-      color_session_text="$color_blue"
-      color_status_text="colour245"
-      color_main="$color_orange"
-      color_secondary="$color_purple"
-      color_level_ok="$color_green"
-      color_level_warn="$color_yellow"
-      color_level_stress="$color_red"
-      color_window_off_indicator="colour088"
-      color_window_off_status_bg="colour238"
-      color_window_off_status_current_bg="colour254"
-
-      tmux_conf_copy_to_os_clipboard=true
-
-      new-session -n $HOST
-      # removing tmux delay
-      set -sg escape-time 1
-
-      # set the base index to start at 1 instead of 0
-      set -g base-index 1
-
-      # set the panes to be base 1 indexed as well
-      setw -g pane-base-index 1
-
-      # setup binding for reloading the tmux config
-      # bind r source-file ~/.tmux.conf |; display "Reloaded!"
-
-      #-------------------------------------------------------#
-      #Pane copy/pasting
-      #-------------------------------------------------------#
-      # unbind [
-      #   bind Escape copy-mode
-      #   unbind p
-      #   bind p paste-buffer
-      #   bind -Tcopy-mode-vi v send -X begin-selection
-      #   bind -Tcopy-mode-vi y send -X copy-selection
-      #-------------------------------------------------------#
-
-
-      # setup prefix forwarding for other applications so that tmux doesn't just capture it and
-      # do nothing with it
-      # bind C-a send-prefix
-
-      #unbind C-b
-      #set -g prefix C-a
-      #bind C-a send-prefix
-
-      setw -g mode-keys vi
-
-      # change the key combinations for vertical (|) and horizontal (-) splitting to make more sense
-      bind | split-window -h
-      bind - split-window -v
-
-      # setup moving between panes to use the VIM movement keys
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-
-      # quick window selection
-      #bind -r C-h select-window -t :-
-      #bind -r C-l select-window -t :+
-
-      # setup pane resizing shortcuts
-      bind -r H resize-pane -L 5
-      bind -r J resize-pane -D 5
-      bind -r K resize-pane -U 5
-      bind -r L resize-pane -R 5
-
-      ####
-
-      set -g terminal-overrides ",alacritty:RGB"
-
-      set -g base-index 1
-      set -g pane-base-index 1
-
-      #### so that i can programatically change title from vim
-      set-option -g set-titles on
-
-      ##### copy to keyboard with vim binding
-      bind-key v split-window -h
-
-      ##### copy to keyboard with vim binding
-      unbind-key -T copy-mode-vi Space     ;   bind-key -T copy-mode-vi v send-keys -X begin-selection
-      unbind-key -T copy-mode-vi Enter     ;   bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
-      unbind-key -T copy-mode-vi C-v       ;   bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      unbind-key -T copy-mode-vi [         ;   bind-key -T copy-mode-vi [ send-keys -X begin-selection
-      unbind-key -T copy-mode-vi ]         ;   bind-key -T copy-mode-vi ] send-keys -X copy-selection
-
-      #### prefix + prefix window swapping
-      bind-key a last-window
+      # Pane and window management
       bind '"' split-window -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
       bind c new-window -c "#{pane_current_path}"
 
-      #### reload config
-      bind r source-file ~/.config/tmux/tmux.conf \; display-message "Config reloaded..."
+      # Other bindings
+      bind a last-window
 
-      #### mouse mode
-      set -g mouse on
-
-      #### send prefix (so C-b works as prefix on remote)
-      bind-key -n C-Tab send-prefix
-
-
-      ## COLORSCHEME: gruvbox dark
-      set-option -g status "on"
-      set-option -g status-position top
-
-      # default statusbar color
-      set-option -g status-style bg=colour22,fg=colour82
-
-      # default window title colors
-      set-window-option -g window-status-style bg=colour22,fg=colour82
-
-      # default window with an activity alert
-      set-window-option -g window-status-activity-style bg=colour22,fg=colour82
-
-      # active window title colors
-      set-window-option -g window-status-current-style bg=colour82,fg=colour22
-
-      # pane border
-      set-option -g pane-active-border-style fg=colour82
-      set-option -g pane-border-style fg=colour22
-
-      # message infos
-      set-option -g message-style bg=colour22,fg=colour82
-
-      # writing commands inactive
-      set-option -g message-command-style bg=colour22,fg=colour82
-
-      # pane number display
-      set-option -g display-panes-active-colour colour82
-      set-option -g display-panes-colour colour22
-
-      # clock
-      set-window-option -g clock-mode-colour colour82
-
-      # bell
-      set-window-option -g window-status-bell-style bg=colour82,fg=colour22
-
-      ## Theme settings mixed with colors (unfortunately, but there is no cleaner way)
-      ## NOTE: there actually may be a cleaner way to set backgrounds on
-      # individual plugins via their own options. see
-      # https://github.com/tmux-plugins/tmux-cpu#customization
-      set-option -g status-justify "left"
-      set-option -g status-left-style none
-      set-option -g status-left-length "80"
-      set-option -g status-right-style none
-      set-option -g status-right-length "80"
-      set-window-option -g window-status-separator ""
-
-      # Установка салатового цвета для различных элементов статуса
-      set-option -g status-left "#[fg=colour82, bg=colour22] #S "
-      set-option -ag status-right "#[fg=colour82, bg=colour22] %l:%M %p %a %d-%b-%y "
-      set-window-option -g window-status-current-format "#[fg=colour22, bg=colour82] #I:#[fg=colour22, bg=colour82, bold] #W #[fg=colour82, bg=colour22, nobold, noitalics, nounderscore]"
-      set-window-option -g window-status-format         "#[fg=colour82, bg=colour22] #I:#[fg=colour82, bg=colour22      ] #W #[fg=colour22, bg=colour22, noitalics]"
-      # END COLORSCHEME
-
+      # Unbind Ctrl+L to prevent window switching
       unbind C-l
       unbind -T root C-l
       unbind -T copy-mode-vi C-l
 
+      # Remote session handling
       if-shell 'test -n "$SSH_CLIENT"' \
-      'source-file /etc/nixos/module/users/bg/tmux/tmux.remote.conf'
+        'source-file /etc/nixos/module/users/bg/tmux/tmux.remote.conf'
 
+      # Toggle key table (modified to work with onedark theme)
       bind -T root M-m \
         set prefix None \;\
         set key-table off \;\
-        set status-style "fg=$color_status_text,bg=$color_window_off_status_bg" \;\
-        set window-status-current-format "#[fg=$color_window_off_status_bg,bg=$color_window_off_status_current_bg]$separator_powerline_right#[default] #I:#W# #[fg=$color_window_off_status_current_bg,bg=$color_window_off_status_bg]$separator_powerline_right#[default]" \;\
-        set window-status-current-style "fg=$color_dark,bold,bg=$color_window_off_status_current_bg" \;\
+        set status-style "fg=#5c6370,bg=#282c34" \;\
+        set window-status-current-format "#[fg=#282c34,bg=#5c6370]#[default] #I:#W# #[fg=#5c6370,bg=#282c34]#[default]" \;\
+        set window-status-current-style "fg=#282c34,bold,bg=#5c6370" \;\
         if -F '#{pane_in_mode}' 'send-keys -X cancel' \;\
-        refresh-client -S \;\
+        refresh-client -S
 
       bind -T off M-m \
         set -u prefix \;\
@@ -206,38 +74,5 @@
         set -u window-status-current-format \;\
         refresh-client -S
     '';
-
-    plugins = with pkgs; [
-      tmuxPlugins.copycat
-      tmuxPlugins.sensible
-      tmuxPlugins.yank
-      {
-        plugin = tmuxPlugins.battery;
-        extraConfig = ''
-          set-option  -g status-right '#[fg=colour246, bg=colour239, nobold, nounderscore, noitalics] batt: #{battery_percentage} '
-        '';
-      }
-      tmuxPlugins.vim-tmux-navigator
-      # tmuxPlugins.prefix-highlight
-      # tmuxPlugins.tmux-update-display # not yet available on nix, but can be loaded with tpm
-      # tmuxPlugins.resurrect
-      # {
-      # plugin = tmuxPlugins.continuum;
-      # extraConfig = ''
-      #   set -g @continuum-restore 'on'
-      #   set -g @continuum-save-interval '60' # minutes
-      # '';
-      # }
-      # {
-      #   plugin = tmuxPlugins.cpu;
-      #   extraConfig = ''
-      #     set -g @cpu_percentage_format " cpu: %3.1f%% "
-      #     set -g @ram_percentage_format " mem: %3.1f%% "
-
-      #     set-option -g status-right '#[fg=colour82, bg=colour22]#{cpu_percentage}'
-      #     set-option -g status-right '#[fg=colour82, bg=colour22]#{ram_percentage}'
-      #   '';
-      # }
-    ];
   };
 }
