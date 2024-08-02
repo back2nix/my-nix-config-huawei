275a70487356ddc274b71363f33d9cc50163097e
back2nix <back2nix@list.ru>
2024-08-02 15:50:55 +0300

    save

--- module/users/bg/home.nix
+++ module/users/bg/home.nix
@@ -226,6 +226,7 @@ in {
       # bottles # Wine Easy-to-use wineprefix manager
       gomodifytags
       simplescreenrecorder
+      pkgs-master.delta # diff tools not only git
     ];
 
     file = {
