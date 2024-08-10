f9bb28357b2fd5eab7e92381147f1f79ed8e5282
back2nix <back2nix@list.ru>
2024-08-10 13:12:09 +0300

    improve print-files

--- module/users/bg/bash/print-files.sh
+++ module/users/bg/bash/print-files.sh
@@ -3,14 +3,14 @@
 print_files() {
   local directory="$1"
   local prefix="$2"
-  local include_extensions="$3"
-  local exclude_extensions="$4"
+  local include_patterns="$3"
+  local exclude_patterns="$4"
 
   for file in "$directory"/*; do
     if [ -d "$file" ]; then
-      print_files "$file" "$prefix$(basename "$file")/" "$include_extensions" "$exclude_extensions"
+      print_files "$file" "$prefix$(basename "$file")/" "$include_patterns" "$exclude_patterns"
     elif [ -f "$file" ]; then
-      if check_extension "$file" "$include_extensions" "$exclude_extensions"; then
+      if check_file "$file" "$include_patterns" "$exclude_patterns"; then
         echo "File: $prefix$(basename "$file")"
         echo '```'
         cat "$file"
@@ -21,31 +21,32 @@ print_files() {
   done
 }
 
-check_extension() {
+check_file() {
   local file="$1"
-  local include_extensions="$2"
-  local exclude_extensions="$3"
-  local ext="${file##*.}"
+  local include_patterns="$2"
+  local exclude_patterns="$3"
+  local filename=$(basename "$file")
+  local ext="${filename##*.}"
 
-  # Если список исключений не пуст и расширение в нем, исключаем файл
-  if [ -n "$exclude_extensions" ]; then
-    IFS=',' read -ra exclude_array <<< "$exclude_extensions"
-    for i in "${exclude_array[@]}"; do
-      if [ "$i" = "$ext" ]; then
+  # Если список исключений не пуст и файл соответствует паттерну, исключаем файл
+  if [ -n "$exclude_patterns" ]; then
+    IFS=',' read -ra exclude_array <<< "$exclude_patterns"
+    for pattern in "${exclude_array[@]}"; do
+      if [[ "$filename" == $pattern || "$ext" == "$pattern" ]]; then
         return 1
       fi
     done
   fi
 
   # Если список включений пуст, включаем все неисключенные файлы
-  if [ -z "$include_extensions" ]; then
+  if [ -z "$include_patterns" ]; then
     return 0
   fi
 
-  # Если список включений не пуст, проверяем наличие расширения в нем
-  IFS=',' read -ra include_array <<< "$include_extensions"
-  for i in "${include_array[@]}"; do
-    if [ "$i" = "$ext" ]; then
+  # Если список включений не пуст, проверяем соответствие файла паттерну
+  IFS=',' read -ra include_array <<< "$include_patterns"
+  for pattern in "${include_array[@]}"; do
+    if [[ "$filename" == $pattern || "$ext" == "$pattern" ]]; then
       return 0
     fi
   done
@@ -54,8 +55,8 @@ check_extension() {
 }
 
 print_usage() {
-  echo "Usage: $0 <directory> [--include|-i file_extensions] [--exclude|-e file_extensions]"
-  echo "Example: $0 /path/to/directory -i go,md,txt -e log,tmp"
+  echo "Usage: $0 <directory> [--include|-i patterns] [--exclude|-e patterns]"
+  echo "Example: $0 /path/to/directory -i go,md,txt,hello.txt -e log,tmp,plugin.nix"
   exit 1
 }
 
@@ -66,17 +67,17 @@ fi
 directory="$1"
 shift
 
-include_extensions=""
-exclude_extensions=""
+include_patterns=""
+exclude_patterns=""
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
     --include|-i)
-      include_extensions="$2"
+      include_patterns="$2"
       shift 2
       ;;
     --exclude|-e)
-      exclude_extensions="$2"
+      exclude_patterns="$2"
       shift 2
       ;;
     *)
@@ -86,4 +87,4 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-print_files "$directory" "" "$include_extensions" "$exclude_extensions"
+print_files "$directory" "" "$include_patterns" "$exclude_patterns"
