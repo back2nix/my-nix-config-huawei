a2367515997b3ce25d2229c45bc39ccdb348d129
back2nix <back2nix@list.ru>
2024-08-20 21:01:33 +0300

    save

--- configuration.nix
+++ configuration.nix
@@ -318,6 +318,8 @@
     # Open ports in the firewall.
     firewall = {
       enable = false;
+      allowedTCPPorts = [18082 18081];
+      allowedUDPPorts = [18082 18081];
       extraCommands = ''
         iptables -t nat -A PREROUTING -i wlp0s20f3 -p tcp --dport 80 -j REDIRECT --to-port 1081
         iptables -t nat -A PREROUTING -i wlp0s20f3 -p tcp --dport 443 -j REDIRECT --to-port 1081
