6a8448a8455a7e017f4e2d9b93869e0afc222188
back2nix <back2nix@list.ru>
2024-12-14 21:15:49 +0300

    mfiles

--- module/users/bg/bash/print-files.sh
+++ module/users/bg/bash/print-files.sh
@@ -1,139 +1,156 @@
 #!/usr/bin/env bash
 
 print_file_content() {
-  local file="$1"
-  echo "File: $file"
-  echo '```'
-  if [ "$keep_comments" = true ]; then
-    if [ "$line_numbers" = true ]; then
-      nl -ba "$file"
+    local file="$1"
+    echo "File: $file"
+    echo '```'
+    if [ "$keep_comments" = true ]; then
+        if [ "$line_numbers" = true ]; then
+            nl -ba "$file"
+        else
+            cat "$file"
+        fi
     else
-      cat "$file"
+        if [ "$line_numbers" = true ]; then
+            sed -e 's/^\s*\/\/.*$//' -e 's/^\s*#.*$//' -e '/^\s*$/d' "$file" | nl -ba
+        else
+            sed -e 's/^\s*\/\/.*$//' -e 's/^\s*#.*$//' -e '/^\s*$/d' "$file"
+        fi
     fi
-  else
-    if [ "$line_numbers" = true ]; then
-      sed -e 's/^\s*\/\/.*$//' -e 's/^\s*#.*$//' -e '/^\s*$/d' "$file" | nl -ba
-    else
-      sed -e 's/^\s*\/\/.*$//' -e 's/^\s*#.*$//' -e '/^\s*$/d' "$file"
-    fi
-  fi
-  echo '```'
-  echo
+    echo '```'
+    echo
 }
 
 print_usage() {
-  echo "Usage: $0 [-i file ...] [-t ext1,ext2,...] [-e pattern ...] [--keep-comments] [-r] [-n]"
-  echo "Example: $0 -i frontend/package.json -t nix,txt,md -e .log -e .tmp -r -n"
-  echo "Use -i to specify files to include"
-  echo "Use -t to specify file extensions to include"
-  echo "Use -e to specify patterns to exclude"
-  echo "Use --keep-comments to preserve comments in the output"
-  echo "Use -r for recursive search"
-  echo "Use -n to show line numbers"
-  exit 1
+    echo "Usage: $0 [-i pattern ...] [-t ext1,ext2,...] [-e pattern ...] [--keep-comments] [-r] [-n]"
+    echo "Example: $0 -i '*.go' -i '*_test.go' -t nix,txt,md -e '*_name.go' -e '.log' -r -n"
+    echo "Use -i to specify files/patterns to include"
+    echo "Use -t to specify file extensions to include"
+    echo "Use -e to specify patterns to exclude"
+    echo "Use --keep-comments to preserve comments in the output"
+    echo "Use -r for recursive search"
+    echo "Use -n to show line numbers"
+    exit 1
 }
 
-include_array=()
+include_patterns=()
 extensions=()
-exclude_array=()
+exclude_patterns=()
 keep_comments=false
 recursive=false
 line_numbers=false
 
 while [[ $# -gt 0 ]]; do
-  case "$1" in
-  -i)
-    include_array+=("$2")
-    shift 2
-    ;;
-  -t)
-    IFS=',' read -ra extensions <<<"$2"
-    shift 2
-    ;;
-  -e)
-    exclude_array+=("$2")
-    shift 2
-    ;;
-  --keep-comments)
-    keep_comments=true
-    shift
-    ;;
-  -r)
-    recursive=true
-    shift
-    ;;
-  -n)
-    line_numbers=true
-    keep_comments=true
-    shift
-    ;;
-  *)
-    echo "Unknown option: $1"
-    print_usage
-    ;;
-  esac
+    case "$1" in
+        -i)
+            include_patterns+=("$2")
+            shift 2
+            ;;
+        -t)
+            IFS=',' read -ra extensions <<<"$2"
+            shift 2
+            ;;
+        -e)
+            exclude_patterns+=("$2")
+            shift 2
+            ;;
+        --keep-comments)
+            keep_comments=true
+            shift
+            ;;
+        -r)
+            recursive=true
+            shift
+            ;;
+        -n)
+            line_numbers=true
+            keep_comments=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            print_usage
+            ;;
+    esac
 done
 
 should_exclude() {
-  local file="$1"
-  for pattern in "${exclude_array[@]}"; do
-    if [[ "$file" =~ $pattern ]]; then
-      return 0
-    fi
-  done
-  return 1
+    local file="$1"
+    for pattern in "${exclude_patterns[@]}"; do
+        if [[ "$(basename "$file")" == ${pattern} ]]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
 has_valid_extension() {
-  local file="$1"
-  local ext="${file##*.}"
-  for valid_ext in "${extensions[@]}"; do
-    if [[ "$ext" == "$valid_ext" ]]; then
-      return 0
-    fi
-  done
-  return 1
+    local file="$1"
+    local ext="${file##*.}"
+    for valid_ext in "${extensions[@]}"; do
+        if [[ "$ext" == "$valid_ext" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+matches_include_pattern() {
+    local file="$1"
+    local basename_file
+    basename_file=$(basename "$file")
+
+    for pattern in "${include_patterns[@]}"; do
+        if [[ "$basename_file" == ${pattern} ]]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
 process_files() {
-  local dir="$1"
-  local find_args=()
-  if [ "$recursive" = true ]; then
-    find_args+=("$dir")
-  else
-    find_args+=("$dir" -maxdepth 1)
-  fi
+    local dir="$1"
+    local find_args=()
 
-  if [ ${#extensions[@]} -gt 0 ]; then
-    find_args+=("(")
-    for ext in "${extensions[@]}"; do
-      find_args+=(-name "*.$ext" -o)
-    done
-    unset 'find_args[${#find_args[@]}-1]'
-    find_args+=(")")
-  fi
-
-  while IFS= read -r -d $'\0' file; do
-    if ! should_exclude "$file"; then
-      print_file_content "$file"
+    if [ "$recursive" = true ]; then
+        find_args+=("$dir")
+    else
+        find_args+=("$dir" -maxdepth 1)
     fi
-  done < <(find "${find_args[@]}" -type f -print0)
+
+    # Handle include patterns
+    if [ ${#include_patterns[@]} -gt 0 ]; then
+        find_args+=("(")
+        for pattern in "${include_patterns[@]}"; do
+            find_args+=(-name "$pattern" -o)
+        done
+        unset 'find_args[${#find_args[@]}-1]'
+        find_args+=(")")
+    fi
+
+    # Handle extensions
+    if [ ${#extensions[@]} -gt 0 ]; then
+        if [ ${#include_patterns[@]} -gt 0 ]; then
+            find_args+=(-o)
+        fi
+        find_args+=("(")
+        for ext in "${extensions[@]}"; do
+            find_args+=(-name "*.$ext" -o)
+        done
+        unset 'find_args[${#find_args[@]}-1]'
+        find_args+=(")")
+    fi
+
+    while IFS= read -r -d $'\0' file; do
+        if ! should_exclude "$file"; then
+            print_file_content "$file"
+        fi
+    done < <(find "${find_args[@]}" -type f -print0 2>/dev/null)
 }
 
-for file in "${include_array[@]}"; do
-  if [ -f "$file" ]; then
-    if ! should_exclude "$file"; then
-      print_file_content "$file"
-    fi
-  else
-    echo "Warning: File not found - $file"
-  fi
-done
-
-if [ ${#extensions[@]} -gt 0 ]; then
-  process_files "."
-fi
-
-if [ ${#include_array[@]} -eq 0 ] && [ ${#extensions[@]} -eq 0 ]; then
-  print_usage
+# Main execution
+if [ ${#include_patterns[@]} -gt 0 ] || [ ${#extensions[@]} -gt 0 ]; then
+    process_files "."
+else
+    print_usage
 fi
