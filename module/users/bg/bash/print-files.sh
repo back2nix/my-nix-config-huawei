a78fe087dd20ef13ac363a70d36448f9f9e873ab
back2nix <back2nix@list.ru>
2024-12-16 10:16:27 +0300

    mfiles

--- module/users/bg/bash/print-files.sh
+++ module/users/bg/bash/print-files.sh
@@ -22,7 +22,7 @@ print_file_content() {
 }
 
 print_usage() {
-    echo "Usage: $0 [-i pattern ...] [-t ext1,ext2,...] [-e pattern ...] [--keep-comments] [-r] [-n]"
+    echo "Usage: $0 [-i pattern ...] [-t ext1,ext2,...] [-e pattern ...] [--keep-comments] [-r] [-n] [-c command]"
     echo "Example: $0 -i '*.go' -i '*_test.go' -t nix,txt,md -e '*_name.go' -e '.log' -r -n"
     echo "Use -i to specify files/patterns to include"
     echo "Use -t to specify file extensions to include"
@@ -30,6 +30,7 @@ print_usage() {
     echo "Use --keep-comments to preserve comments in the output"
     echo "Use -r for recursive search"
     echo "Use -n to show line numbers"
+    echo "Use -c 'command' to use output of command as source of files (e.g. -c \"rg -l 'pattern'\")"
     exit 1
 }
 
@@ -39,6 +40,7 @@ exclude_patterns=()
 keep_comments=false
 recursive=false
 line_numbers=false
+command=""
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -67,6 +69,10 @@ while [[ $# -gt 0 ]]; do
             keep_comments=true
             shift
             ;;
+        -c)
+            command="$2"
+            shift 2
+            ;;
         *)
             echo "Unknown option: $1"
             print_usage
@@ -77,13 +83,23 @@ done
 should_exclude() {
     local file="$1"
     for pattern in "${exclude_patterns[@]}"; do
-        # Если паттерн содержит путь
+        local abs_file
+        if [[ "$file" = /* ]]; then
+            abs_file="$file"
+        else
+            abs_file="$(pwd)/$file"
+        fi
         if [[ "$pattern" == *"/"* ]]; then
-            if [[ "$file" == $pattern ]]; then
+            local full_pattern
+            if [[ "$pattern" = /* ]]; then
+                full_pattern="$pattern"
+            else
+                full_pattern="$(pwd)/$pattern"
+            fi
+            if [[ "$abs_file" == $full_pattern ]] || [[ "$abs_file" == *"/$pattern" ]]; then
                 return 0
             fi
         else
-            # Для паттернов без пути проверяем только имя файла
             local basename_file
             basename_file=$(basename "$file")
             if [[ "$basename_file" == $pattern ]]; then
@@ -108,14 +124,11 @@ has_valid_extension() {
 matches_include_pattern() {
     local filepath="$1"
     for pattern in "${include_patterns[@]}"; do
-        # Если паттерн содержит путь, проверяем относительно корня поиска
         if [[ "$pattern" == *"/"* ]]; then
-            # Для паттернов с путём, проверяем как точное совпадение, так и glob
             if [[ "$filepath" == $pattern ]] || [[ "$filepath" == *"/$pattern" ]]; then
                 return 0
             fi
         else
-            # Для паттернов без пути проверяем только имя файла
             local basename_file
             basename_file=$(basename "$filepath")
             if [[ "$basename_file" == $pattern ]]; then
@@ -126,50 +139,69 @@ matches_include_pattern() {
     return 1
 }
 
-process_files() {
-    # Определяем корневую директорию для поиска
-    local search_root
-    # Если в шаблонах есть путь, используем его как основу
-    for pattern in "${include_patterns[@]}"; do
-        if [[ "$pattern" == *"/"* ]]; then
-            search_root=$(dirname "$pattern")
-            break
-        fi
-    done
-    # Если путь не найден, используем текущую директорию
-    search_root=${search_root:-.}
-
-    local find_cmd="find"
-    if [ "$recursive" = true ]; then
-        find_cmd="find $search_root"
-    else
-        find_cmd="find $search_root -maxdepth 1"
-    fi
-
-    local ext_condition=""
-    if [ ${#extensions[@]} -gt 0 ]; then
-        ext_condition="("
-        for ext in "${extensions[@]}"; do
-            ext_condition+=" -name '*.$ext' -o"
-        done
-        ext_condition=${ext_condition%-o}
-        ext_condition+=" )"
-    fi
-
+process_command_output() {
+    local cmd="$1"
     while IFS= read -r file; do
-        file="${file#./}"
-        if [ ${#include_patterns[@]} -eq 0 ] || matches_include_pattern "$file"; then
-            if ! should_exclude "$file"; then
-                if [ ${#extensions[@]} -eq 0 ] || has_valid_extension "$file"; then
-                    print_file_content "$file"
-                fi
+        if ! should_exclude "$file"; then
+            if [ ${#extensions[@]} -eq 0 ] || has_valid_extension "$file"; then
+                print_file_content "$file"
             fi
         fi
-    done < <(eval "$find_cmd -type f $ext_condition")
+    done < <(eval "$cmd")
 }
 
-# Main execution
-if [ ${#include_patterns[@]} -gt 0 ] || [ ${#extensions[@]} -gt 0 ]; then
+process_files() {
+    local has_path_pattern=false
+    local search_roots=(".")
+
+    for pattern in "${include_patterns[@]}"; do
+        if [[ "$pattern" == *"/"* ]]; then
+            has_path_pattern=true
+            local dir=$(dirname "$pattern")
+            if [[ ! " ${search_roots[@]} " =~ " ${dir} " ]]; then
+                search_roots+=("$dir")
+            fi
+        fi
+    done
+
+    if [ "$has_path_pattern" = false ]; then
+        search_roots=(".")
+    fi
+
+    for search_root in "${search_roots[@]}"; do
+        local find_cmd="find"
+        if [ "$recursive" = true ]; then
+            find_cmd="find $search_root"
+        else
+            find_cmd="find $search_root -maxdepth 1"
+        fi
+
+        local ext_condition=""
+        if [ ${#extensions[@]} -gt 0 ]; then
+            ext_condition="("
+            for ext in "${extensions[@]}"; do
+                ext_condition+=" -name '*.$ext' -o"
+            done
+            ext_condition=${ext_condition%-o}
+            ext_condition+=" )"
+        fi
+
+        while IFS= read -r file; do
+            file="${file#./}"
+            if [ ${#include_patterns[@]} -eq 0 ] || matches_include_pattern "$file"; then
+                if ! should_exclude "$file"; then
+                    if [ ${#extensions[@]} -eq 0 ] || has_valid_extension "$file"; then
+                        print_file_content "$file"
+                    fi
+                fi
+            fi
+        done < <(eval "$find_cmd -type f $ext_condition")
+    done
+}
+
+if [ -n "$command" ]; then
+    process_command_output "$command"
+elif [ ${#include_patterns[@]} -gt 0 ] || [ ${#extensions[@]} -gt 0 ]; then
     process_files "."
 else
     print_usage
