f597ad4a86d17944241b00d1c2ed0717ea7df8f9
back2nix <back2nix@list.ru>
2025-11-12 22:48:56 +0300

    cuda cache

--- devices/desktop/default.nix
+++ devices/desktop/default.nix
@@ -1,11 +1,17 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   imports = [
     ./hardware-configuration.nix
   ];
+
+  nix.settings = {
+    substituters = lib.mkBefore [ "https://cuda-maintainers.cachix.org" ];
+    trusted-public-keys = lib.mkBefore [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
+  };
 
   networking.hostName = "desktop";
 
