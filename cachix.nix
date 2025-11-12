04d553fdc51d0625324b58b915e74830af1df9fb
back2nix <back2nix@list.ru>
2025-11-13 01:24:11 +0300

    cachix

--- cachix.nix
+++ cachix.nix
@@ -10,8 +10,24 @@
   imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
 in {
   inherit imports;
-  nix.settings.substituters = [
-    "https://cache.nixos.org/"
-    "https://robotnix.cachix.org"
-  ];
+  nix.settings = {
+    substituters = [
+      "https://cache.garnix.io/"
+      "https://cache.nixos.org/"
+      "https://robotnix.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    trusted-substituters = [
+      "cache.nixos.org"
+      "nix-community.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
 }
