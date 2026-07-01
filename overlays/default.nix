bfcd4fa7b3be4d5c1fa52babfe5505f06a8ac79e
back2nix <back2nix@list.ru>
2026-07-01 23:24:39 +0300

    update claude

--- overlays/default.nix
+++ overlays/default.nix
@@ -96,13 +96,13 @@
       #     "$@"
       # '';
 
-      # --- НАЧАЛО: Обновление claude-code до 2.1.196 ---
+      # --- НАЧАЛО: Обновление claude-code до 2.1.197 ---
       claude-code = prev.stdenvNoCC.mkDerivation {
         pname = "claude-code";
-        version = "2.1.196";
+        version = "2.1.197";
         src = prev.fetchurl {
-          url = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/2.1.196/linux-x64/claude";
-          sha256 = "eb933c6dd5534db89b83ba09009d5c0932bd1395f7e3bb0f34ba37eec37bbade";
+          url = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/2.1.197/linux-x64/claude";
+          sha256 = "f54e69cbc89b2da61a415700af7ff52a147e862517d4f1b0eecf768448cf7f83";
         };
         dontUnpack = true;
         dontBuild = true;
@@ -122,7 +122,7 @@
         '';
         meta.mainProgram = "claude";
       };
-      # --- КОНЕЦ: Обновление claude-code до 2.1.196 ---
+      # --- КОНЕЦ: Обновление claude-code до 2.1.197 ---
 
       # --- НАЧАЛО: Обновление gemini-cli до 0.44.0-nightly.20260518.g5611ff40e ---
       gemini-cli = final.unstable.gemini-cli.overrideAttrs (oldAttrs: rec {
